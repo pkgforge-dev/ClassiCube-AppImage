e7ea1a49164ccebe6882e6848b0015a7b6f7e4a5
Link4Electronics <link4electronics@gmail.com>
2026-02-03 04:09:26 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,30 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    libdecor \
+    openal   \
+    sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of ClassiCube..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/UnknownShadow200/ClassiCube"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./ClassiCube
+echo "$VERSION" > ~/version
+
+cd ./ClassiCube
+make -j$(nproc) RELEASE=1 BUILD_SDL3=1
+
+install -D -m755 "ClassiCube/ClassiCube" "/usr/bin/classicube"
+install -D -m644 "ClassiCube/license.txt" "/usr/share/licenses/classicube/license.txt"
+install -D -m644 "ClassiCube/misc/CCicon.png" "/usr/share/classicube/CCicon.png"
+#install -D -m644 "classicube.desktop" "/usr/share/applications/classicube.desktop"
