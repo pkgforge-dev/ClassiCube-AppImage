808e50ae753ac023952ba003d2e60cc90d3af2aa
Link4Electronics <link4electronics@gmail.com>
2026-02-03 04:09:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,13 +3,13 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q classicube-git | awk '{print $2; exit}') # example command to get version of application here
+#VERSION=$(pacman -Q classicube-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/classicube-git/CCicon.png
-export DESKTOP=/usr/share/applications/classicube.desktop
+export ICON=/usr/share/classicube/CCicon.png
+#export DESKTOP=/usr/share/applications/classicube.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
