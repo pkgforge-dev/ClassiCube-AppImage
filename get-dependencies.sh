a0bf06c4e41dcf501cc92e8803b6da8eaac30cd3
Link4Electronics <link4electronics@gmail.com>
2026-05-15 19:07:31 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,29 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor   \
-    openal     \
-    sdl3       \
-    ttf-dejavu
+pacman -Syu --noconfirm openal sdl3 ttf-dejavu
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
-echo "Making nightly build of ClassiCube..."
+echo "Building ClassiCube..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/UnknownShadow200/ClassiCube"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./ClassiCube
+git clone --depth 1 "$REPO" ./ClassiCube
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./ClassiCube
 make -j$(nproc) RELEASE=1 BUILD_SDL3=1
 mv -v "ClassiCube" ../AppDir/bin
-cp -r misc/CCicon.png ../AppDir
+cp -v misc/CCicon.png ../AppDir
