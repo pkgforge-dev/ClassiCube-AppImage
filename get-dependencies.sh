6f42704f78053f11b2ac17634e7ed21a8e7795ea
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:55:28 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,11 +16,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
-echo "Making nightly build of ClassiCube..."
+echo "Building ClassiCube..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/UnknownShadow200/ClassiCube"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
